a38dadb81c4d45d5b921b0ef3bf62945a80420b2
tech-gian <sdi1900059@di.uoa.gr>
2021-02-18 00:34:40 +0200

    Initial commit, Virtual_Keyboard

---
+++ Virtual_Keyboard/vir_keyboard.js
@@ -0,0 +1,13 @@
+
+
+// When key is pressed, add this character to input_form
+function key(button_text) {
+
+}
+
+
+
+// Links
+// https://hackerthemes.com/bootstrap-cheatsheet/
+// https://virtual-keyboard.js.org/
+
